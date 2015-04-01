4b862734f1b251d494a6dd53bc784427775cc9f1
RoddChames <Webmaster@Roddrecuss-MacBook-Pro.local>
2015-03-31 21:18:05 -0400

    Playing with Code
    Just playing around with code to familiarize myself with github.

---
+++ Chames_Roddrecus_WPF/Setup Github/First Github Project/js/script.js
@@ -0,0 +1,19 @@
+/*
+ Rodd Chames
+ Test js file
+ */
+
+//alert("Testing 1,2,3");
+
+console.log("Testing the console.log");
+
+var userInput = prompt("Type in your name:");
+console.log(userInput);
+
+
+var x = document.getElementById("chames");
+function myFunction() {
+    x.style.color = "blue";
+    x.style.fontSize = "80px";
+};
+
