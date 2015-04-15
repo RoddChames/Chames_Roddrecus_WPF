d8188b92e85bc905eca79cd03027a9f1d0f09e41
RoddChames <Webmaster@Roddrecuss-MacBook-Pro.local>
2015-04-14 23:13:32 -0400

    Sparky
    In this portion I solved the problem for how old Sparky is based on
    human years.

---
+++ Expressions_Worksheet/js/script.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Roddrecus Chames  on 4/14/15.
+ */
+
+//Dogs age 7 times faster than humans so a dog that is 1 year old in human years is 7 years old in “dog years.”  Calculate how old Sparky the pit bull is in dog years based on his actual age.
+
+//Givens:
+//Sparky’s age.
+
+//Result Variable: Sparky’s age in dog years.
+
+
+//Result to Print: “Sparky is X human years old which is X in dog years.
+
+var humanYears = 4;             //This is Sparkies age in human years. (I gave him this age since it wasn't told to us)
+var dogYears = humanYears * 7;  //We were told that each year for a human is seven years for a dog.  So humanYears x 7 will give us the product.
+console.log("Sparky is " + dogYears + " years old.");          //This prints out "Sparky is 28 years old."
+
+
+
