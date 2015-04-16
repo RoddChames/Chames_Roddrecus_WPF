f8580ca463a06e1c31545ef3423025d79f471dbb
RoddChames <Webmaster@Roddrecuss-MacBook-Pro.local>
2015-04-16 19:05:20 -0400

    Greeting
    Created a greeting to gather customer’s name and informed customer of
    why we requested his/her name.

--- Chames_Roddrecus_Expressions_Assignment/js/script.js
+++ Chames_Roddrecus_Expressions_Assignment/js/script.js
@@ -3,5 +3,9 @@
  */
 
 //Testing to see if my js file is linked to my index.html file
-  alert("This is only a test");
+ alert("Here is a calculator to help you add up earned vacation days.");
+
+var customerName = prompt("Please enter your name so we can better assist you in calculating your vacation days.");
+
+alert("Hello " + customerName + " I hope your day is going well thus far!!! \nPlease click on ok to continue?");
 
