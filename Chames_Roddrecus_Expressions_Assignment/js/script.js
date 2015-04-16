6323d7b0a24c3930152b567fd53919120d26247f
RoddChames <Webmaster@Roddrecuss-MacBook-Pro.local>
2015-04-16 18:58:08 -0400

    Deleted
    I deleted all previous work and started over.  I used the alert to test
    the functionality of my js file to my index file.

--- Chames_Roddrecus_Expressions_Assignment/js/script.js
+++ Chames_Roddrecus_Expressions_Assignment/js/script.js
@@ -3,26 +3,5 @@
  */
 
 //Testing to see if my js file is linked to my index.html file
-// Test was successful so I blocked it out using this note format.  alert("This is only a test");
-
-//My calculator will calculate the total amount of leave days a person in the military will accumulate over a time period
-
-//First I want to collect a name and inform the person as to why I need it.
-var nameInput = prompt("Let's figure out your projected leave days. \nPlease type your name to begin");
-
-//console.log() will help me to identify if my variable is recognized in the inspection console.
-console.log(nameInput);
-
-//this alert is just a friendly gesture before we get started.
-alert("Hello " + nameInput + " I hope your day is going well! \nLet's get started");
-
-//prompt for job type (Civilian, Navy, or Marine)
-prompt("Are you labeled as a Civilian Contractor, Navy Personnel, or Marine?");
-
-var months = prompt("How many months have you been employed?")
-
-//These are the number days of leave earned each year by job type (choices will be: Civilian, Navy, or Marine).
-var leaveEarnedByJob = [1.5 * "", 2.5 * 12, 3.5 * 12];
-
-alert(leaveEarnedByJob[0]);
+  alert("This is only a test");
 
