de84e93371a6c039688a8730e994bb39c03d024e
RoddChames <Webmaster@Roddrecuss-MacBook-Pro.local>
2015-04-15 21:34:57 -0400

    Name Collected
    I have taken the steps necessary to collect information from the user
    while notifying them of why I need it.

--- Chames_Roddrecus_Expressions_Assignment/js/script.js
+++ Chames_Roddrecus_Expressions_Assignment/js/script.js
@@ -3,4 +3,16 @@
  */
 
 //Testing to see if my js file is linked to my index.html file
-alert("This is only a test");
+// Test was successful so I blocked it out using this note format.  alert("This is only a test");
+
+//My calculator will calculate the total amount of leave days a person in the military will accumulate over a time period
+
+//First I want to collect a name and inform the person as to why I need it.
+var nameInput = prompt("Let's figure out your projected leave days. \nPlease type your name to begin");
+
+//console.log() will help me to identify if my variable is recognized in the inspection console.
+console.log(nameInput);
+
+//this alert is just a friendly gesture before we get started.
+alert("Hello " + nameInput + " I hope your day is going well! \nLet's get started");
+
