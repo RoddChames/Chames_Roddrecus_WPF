2d531851f0795af73dc2eca9072821276f38c689
RoddChames <Webmaster@Roddrecuss-MacBook-Pro.local>
2015-04-20 00:26:57 -0400

    eating contest
    completed conditional operations for the eating contest.

--- Conditionals_Worksheet/js/script.js
+++ Conditionals_Worksheet/js/script.js
@@ -2,4 +2,16 @@
  * Created by Webmaster on 4/20/15.
  */
 
-alert("Connection Test");
+
+//weight required for contest
+var reqWeight = 250;
+
+//weight of contestant
+var contestantWeight = 251;
+
+//if contestant does not weigh a minimum of 250 lbs then they cannot participate in contest
+if(contestantWeight < reqWeight) {
+    console.log("No contest for you!");
+}else {
+    console.log("You are ready for the contest!");
+}
