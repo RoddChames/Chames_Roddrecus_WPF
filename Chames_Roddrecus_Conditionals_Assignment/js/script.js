2d2d314d54831dc7e2013ab5be81c2eb1c53a2ec
RoddChames <Webmaster@Roddrecuss-MacBook-Pro.local>
2015-04-20 22:57:57 -0400

    Array
    Created array for 3 military branches.

--- Chames_Roddrecus_Conditionals_Assignment/js/script.js
+++ Chames_Roddrecus_Conditionals_Assignment/js/script.js
@@ -4,4 +4,14 @@
  */
 
 //Testing link between .js and index file
-alert("This is to test js");
+//alert("This is to test js"); Test checked good, so I turned the alert into a note.
+
+//This calculator will tell how many bombs need to be built daily for combat operation by military branch
+
+//Different military branches
+var militaryBranches = ["Navy", "Marines", "Airforce"];
+
+//printed out military branches to make sure arrays worked properly
+    console.log(militaryBranches);
+
+
