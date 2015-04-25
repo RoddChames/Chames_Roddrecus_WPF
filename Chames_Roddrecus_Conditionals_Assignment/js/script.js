ca51270dd48a9980a8aedd77bda786962a4199cd
RoddChames <Webmaster@Roddrecuss-MacBook-Pro.local>
2015-04-25 02:18:09 -0400

    still having trouble
    must figure out what I’m doing wrong.

--- Chames_Roddrecus_Conditionals_Assignment/js/script.js
+++ Chames_Roddrecus_Conditionals_Assignment/js/script.js
@@ -4,113 +4,22 @@
  */
 
 //Testing link between .js and index file
-//alert("This is to test js"); Test checked good, so I turned the alert into a note.
+alert("This calculator tells the minimum number of bombs that need to be assembled based on branch of service. ")
 
-//This calculator will tell how many bombs (minimum) need to be built daily by branch based on if they are deployed or training
+var bombsByBranch = [100,200,300];
 
-/*
-//Requesting military branch
-var membersBranch = prompt("To allow us to better serve you please choose your military organization. (Navy, Marines, or Airforce)");
+var serviceBranch = ["Airforce","Marines","Navy"];
 
-//Alert to choice of service
-alert("Thank you. You chose " + membersBranch + ". Please select ok to continue.");
-
-//minimum navy bombs while deployed
-var navyBombsDeployed = 450;
-
-//minimum navy bombs while training
-var navyBombsTraining = 123;
-
-//minimum marine bombs deployed
-var marineBombsDeployed = 375;
-
-//minimum marine bombs training
-var marineBombsTraining = 150;
-
-//minimum airforce bombs deployed
-var airforceBombsDeployed = 250;
-
-//minimum airforce bombs training
-var airforceBombsTraining = 90;
-
-//finding out if person is deployed or training
-var opStatus = prompt("Are you currently involed in training evolution, or deployed?");
-
-//ask users for number of bombs built
-var prompt1 = prompt("Please enter number of bombs built.");
-
-//verify that the opstatus variable is working
-console.log(opStatus);
-
-
-
-
-
-
-if((membersBranch == "Navy" && opStatus == "deployed") && prompt1 > navyBombsDeployed ) {
-    console.log("You are ready for war!!!");
-}
-
-if((membersBranch == "Marines" && opStatus == "deployed") && prompt1 > marineBombsDeployed ) {
-    console.log("You are ready for war!!!");
-}else{
-    console.log("please verify information.")
-}
-*/
-
-
-
-
-
-
-
-
-
-
-/*New section based on goToTraining Video*/
-
-//Testing link between .js and index file
-//alert("This is to test js"); Test checked good, so I turned the alert into a note.
-
-//This calculator will tell how many bombs (minimum) need to be built daily by branch based on if they are deployed or training
-
-alert("This calculator will help you determine the minimum bombs required for your daily operational commitment.  To continue please click ok.");
-
-var bombsRequired = ["450","123","375"];
-
-var prompt1 = prompt("To allow us to better serve you please choose your military organization. (Navy, Marines, or Airforce)");
+var prompt1 = prompt("What branch of the service are you a member of? (Navy, Marines, Airforce)");
 console.log(prompt1);
 
-prompt2 = prompt("Are you currently deployed or training?");
-console.log(prompt2);
 
-if(prompt1 === "Navy" && prompt2 ==="deployed") {
-    console.log(bombsRequired[0]);
-   var prompt3 = prompt("How many bombs have you assembled?");
-}
-
-if(prompt3 < bombsRequired[0]){
-    console.log("please assemble a minimum of " + bombsRequired[0] + " bombs to meet your operational quota for today.");
+if(prompt1 === serviceBranch[0]||"airforce"){
+    console.log("The " +serviceBranch[0]+ " requires a minimum of " +bombsByBranch[0]+ " bombs to be assembled.");
+    alert("The " +serviceBranch[0]+ " requires a minimum of " +bombsByBranch[0]+ " bombs to be assembled.");
 }else{
-    console.log("You have met your your operational quota for today.");
+    prompt1 = prompt("This entry is not valid");
+    console.log("Entry not valid");
 }
-
-
-//Airforce
-if(prompt1 === "Airforce" && prompt2 ==="deployed") {
-    console.log(bombsRequired[1]);
-    prompt3 = prompt("How many bombs have you assembled?");
-}
-
-if(prompt3 < bombsRequired[1]){
-    console.log("please assemble a minimum of " + bombsRequired[1] + " bombs to meet your operational quota for today.");
-}else{
-    console.log("You have met your your operational quota for today.");
-}
-
-
-
-
-
 
 
