9fc7780cfbc0811f58d0117b926dc3e6e1fedc2b
RoddChames <Webmaster@Roddrecuss-MacBook-Pro.local>
2015-04-26 10:32:33 -0400

    Completed Airforce
    I have completed all codes for the Airforce service branch. Next step
    will be entering notes for my code.

--- Chames_Roddrecus_Conditionals_Assignment/js/script.js
+++ Chames_Roddrecus_Conditionals_Assignment/js/script.js
@@ -4,7 +4,7 @@
  */
 
 //Testing link between .js and index file
-alert("This calculator tells the minimum number of bombs that need to be assembled based on branch of service. ")
+alert("This calculator tells the minimum number of bombs that need to be assembled based on branch of service. ");
 
 var bombsByBranch = [100,200,300];
 
@@ -14,12 +14,40 @@ var prompt1 = prompt("What branch of the service are you a member of? (Navy, Mar
 console.log(prompt1);
 
 
-if(prompt1 === serviceBranch[0]||"airforce"){
-    console.log("The " +serviceBranch[0]+ " requires a minimum of " +bombsByBranch[0]+ " bombs to be assembled.");
-    alert("The " +serviceBranch[0]+ " requires a minimum of " +bombsByBranch[0]+ " bombs to be assembled.");
+
+if(prompt1 === "Airforce") {
+    console.log("The " + serviceBranch[0] + " requires a minimum of " + bombsByBranch[0] + " bombs to be assembled.");
 }else{
-    prompt1 = prompt("This entry is not valid");
-    console.log("Entry not valid");
+    console.log("Invalid entry. Please enter valid input.");
+    var prompt2 = prompt("Invalid entry. Please enter valid input.");
+    console.log(prompt2);
+    if(prompt2 === "Airforce"){
+        console.log("The " + serviceBranch[0] + " requires a minimum of " + bombsByBranch[0] + " bombs to be assembled.");
+    }
 }
+
+var prompt1 = prompt("How many bombs did you assemble for today's project?");
+console.log(prompt1);
+
+if(prompt1 >= bombsByBranch[0]) {
+    console.log("You have accomplished the minimum ammount of assembled bombs for today's evolution.");
+    alert("You have accomplished the minimum ammount of assembled bombs for today's evolution.");
+}else {
+    console.log("The " + serviceBranch[0] + " requires a minimum of " + bombsByBranch[0] + " bombs to be assembled.");
+    var prompt2 = prompt("You have more work to do. \nThe " + serviceBranch[0] + " requires a minimum of " + bombsByBranch[0] + " bombs to be assembled.\nPlease enter a different value");
+    console.log(prompt2);
+    if (prompt2 <= bombsByBranch[0]) {
+        console.log("You have accomplished the minimum ammount of assembled bombs for today's evolution.");
+        alert("You have accomplished the minimum ammount of assembled bombs for today's evolution.");
+    }
+}
+
+
+
+
+
+
+
+
 
 
