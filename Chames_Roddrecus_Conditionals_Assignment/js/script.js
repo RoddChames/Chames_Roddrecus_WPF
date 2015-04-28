ea6571a2fbdfb3f7f401651d6649404f44f91e03
RoddChames <Webmaster@Roddrecuss-MacBook-Pro.local>
2015-04-27 20:48:36 -0400

    completed using instructor tips
    I have completed the assignment with the tips from the instructor, but
    I have to find a way to include a ternary string within my code.

--- Chames_Roddrecus_Conditionals_Assignment/js/script.js
+++ Chames_Roddrecus_Conditionals_Assignment/js/script.js
@@ -14,86 +14,43 @@ var serviceBranch = ["Airforce","Marines","Navy"];
 
 //Prompt will find out what branch of service the user is in.
 var question1 = prompt("What branch of the service are you a member of? (Navy, Marines, Airforce)");
-
-//prints out input from question 1.
 console.log(question1);
 
-//////////// Start of conditional coding for Airforce. ////////////
-
-//Ensures user enters Airforce in prompt.
-if(question1 === serviceBranch[0]) {
-//if(question1 === serviceBranch[1]) {
-//if(question1 === serviceBranch[2]) {
-
-
-    //based on the user's input this prints out how many bombs are required for their respective branches.
-    console.log("The " + serviceBranch[0] + " requires a minimum of " + bombsByBranch[0] + " bombs to be assembled.");
-    //console.log("The " + serviceBranch[1] + " requires a minimum of " + bombsByBranch[1] + " bombs to be assembled.");
-    //console.log("The " + serviceBranch[3] + " requires a minimum of " + bombsByBranch[3] + " bombs to be assembled.");
-
-//If user enters anything other than Airforce in the prompt then the following code will initialize.
+if(question1 === serviceBranch[0]){
+    console.log("The " + serviceBranch[0] + " requires a minimum of " + bombsByBranch[0] + " to be assembled to complete this evolution.");
+}else if(question1 === serviceBranch[1]){
+    console.log("The " + serviceBranch[1] + " requires a minimum of " + bombsByBranch[1] + " to be assembled to complete this evolution.");
+}else if(question1 === serviceBranch[2]){
+    console.log("The " + serviceBranch[2] + " requires a minimum of " + bombsByBranch[2] + " to be assembled to complete this evolution.");
 }else{
-
-    //prints out that an invalid entry was entered in the prompt.
-    console.log("Invalid entry. Please enter valid input.");
-
-    //This is a second chance for user to input valid info.
-    var prompt2 = prompt("Invalid entry. Please enter valid input.");
-
-    //This prints out the input of the above prompt.
-    console.log(prompt2);
-
-    //This ensures that after a second attempt to input valid information user is able to pick up where they left off.
-    if(prompt2 === serviceBranch[0]){
-    //if(prompt2 === serviceBranch[1]){
-    //if(prompt2 === serviceBranch[2]){
-
-        //Based on the user's input this prints out how many bombs are required for their respective branches.(initiated on second try).
-        console.log("The " + serviceBranch[0] + " requires a minimum of " + bombsByBranch[0] + " bombs to be assembled.");
-        //console.log("The " + serviceBranch[1] + " requires a minimum of " + bombsByBranch[1] + " bombs to be assembled.");
-        //console.log("The " + serviceBranch[2] + " requires a minimum of " + bombsByBranch[2] + " bombs to be assembled.");
-    }
+    console.log("Please select a valid branch of service.\nWhat branch of the service are you a member of? (Navy, Marines, Airforce)");
+    question1 = prompt("Please select a valid branch of service.\nWhat branch of the service are you a member of? (Navy, Marines, Airforce)");
+    console.log(question1);
 }
 
-//The user inputs how many bombs they assembled.
-var question2 = prompt("How many bombs did you assemble for today's project?");
 
-//Prints out the number the user inputs
-console.log(question2);
+var question2 = prompt("How many bombs have you assembled for today's training?");
+    console.log(question2);
 
-//This conditional tracks whether the user has assembled the minimum required bombs.
-if(question2 >= bombsByBranch[0]) {
-//if(question2 >= bombsByBranch[1]) {
-//if(question2 >= bombsByBranch[2]) {
-
-    //Logs the input from the prompt.
-    console.log("You have accomplished the minimum amount of assembled bombs for today's evolution.");
-
-    //This alert is triggered when the user inputs a sufficient amount of bombs.
-    alert("You have accomplished the minimum amount of assembled bombs for today's evolution.");
-
-//The else statement is only initialized if the user doesn't put in the minimum number required by the branch of service.
-}else {
-
-    //This will print only when the user inputs a number below the minimum required.
-    console.log("The " + serviceBranch[0] + " requires a minimum of " + bombsByBranch[0] + " bombs to be assembled.");
-    //console.log("The " + serviceBranch[1] + " requires a minimum of " + bombsByBranch[1] + " bombs to be assembled.");
-    //console.log("The " + serviceBranch[2] + " requires a minimum of " + bombsByBranch[2] + " bombs to be assembled.");
-
-
-    //This prompt is the second chance for the user to input more bombs if needed.
-    var prompt2 = prompt("You have more work to do. \nThe " + serviceBranch[0] + " requires a minimum of " + bombsByBranch[0] + " bombs to be assembled.\nPlease enter a different value");
-    //var prompt2 = prompt("You have more work to do. \nThe " + serviceBranch[1] + " requires a minimum of " + bombsByBranch[1] + " bombs to be assembled.\nPlease enter a different value");
-    //var prompt2 = prompt("You have more work to do. \nThe " + serviceBranch[2] + " requires a minimum of " + bombsByBranch[2] + " bombs to be assembled.\nPlease enter a different value");
-
-
-    //This prints the amount when a second chance is given to the user.
-    console.log(prompt2);
-
-    //This ensures that the second chance given to the user is greater than or equal to the minimum bomb assembly required. This line also prints out that the user has completed the daily tasks, and alerts the user that they have completed the daily tasks.
-    (prompt2 <= bombsByBranch[0]) ?  console.log("You have accomplished the minimum amount of assembled bombs for today's evolution.") : alert("You did not complete todays evolution");
-    //(prompt2 <= bombsByBranch[1]) ?  console.log("You have accomplished the minimum amount of assembled bombs for today's evolution.") : alert("You did not complete todays evolution");
-    //(prompt2 <= bombsByBranch[2]) ?  console.log("You have accomplished the minimum amount of assembled bombs for today's evolution.") : alert("You did not complete todays evolution");
-
+if(question1 === serviceBranch[0] && question2 >= bombsByBranch[0]){
+    console.log("You have successfully assembled the minimum number of bombs required for today's training evolution. Enjoy the rest of your day!");
+}else if(question1 === serviceBranch[1] && question2 >= bombsByBranch[1]){
+    console.log("You have successfully assembled the minimum number of bombs required for today's training evolution. Enjoy the rest of your day!");
+}else if(question1 === serviceBranch[2] && question2 >= bombsByBranch[2]){
+    console.log("You have successfully assembled the minimum number of bombs required for today's training evolution. Enjoy the rest of your day!");
+}else{
+    console.log("You have not assembled the minimum number of bombs required by your branch of service.\nPlease continue training.");
+    var question2 = prompt("Please build the minimum number of bombs required by your branch of service.\nPlease enter new amount.");
+    console.log(question2);
 }
 
+if(question1 === serviceBranch[0] && question2 >= bombsByBranch[0]){
+    console.log("You have successfully assembled the minimum number of bombs required for today's training evolution. Enjoy the rest of your day!");
+}else if(question1 === serviceBranch[1] && question2 >= bombsByBranch[1]){
+    console.log("You have successfully assembled the minimum number of bombs required for today's training evolution. Enjoy the rest of your day!");
+}else if(question1 === serviceBranch[2] && question2 >= bombsByBranch[2]){
+    console.log("You have successfully assembled the minimum number of bombs required for today's training evolution. Enjoy the rest of your day!");
+}else{
+    console.log("Please contact our help desk for more assistance.");
+    alert("Please contact our help desk for more assistance.");
+}
