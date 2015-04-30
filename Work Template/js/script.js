1ff61f15b3a76d7635bcb821e33eec9f82f9b6ce
RoddChames <Webmaster@Roddrecuss-MacBook-Pro.local>
2015-04-29 21:09:51 -0400

    info and prices
    I have collected the users name, and I have also set up prices for each
    vehicle in the list.

--- Work Template/js/script.js
+++ Work Template/js/script.js
@@ -5,13 +5,30 @@
 
 alert("This calculator will help determine how much car payments would be for a new car.");
 
-var carChoices = ["Dodge Charger","Chevy Camaro","Ford Mustang"];
+var question1 = prompt("Please enter your name to help us better serve you");
+console.log(question1);
+
+var question2 = prompt("Hello " +question1+ " what kind of car would you like to purchase?\n(Dodge Charger, Chevy Camaro, Ford Mustang)").toLowerCase();
+console.log(question2);
+
+if(question2 === "dodge charger" ){
+    console.log("The price the selected car is $" +42,500+ ".");
+}else if(question2 === "chevy camaro"){
+    console.log("The price the selected car is $" +38,200+ ".");
+}else if(question2 === "ford mustang"){
+    console.log("The price the selected car is $" +37,800+ ".");
+}
+
+while(){
+
+}
+
+
+/*var carChoices = ["Dodge Charger","Chevy Camaro","Ford Mustang"];
 
 function monthlyPay(payByHour,hoursPerWeek){
     var payByWeek = payByHour * hoursPerWeek;
     var payPerMonth = payByWeek * 4;
     return payPerMonth;
 }
-
-var totalMonthlyPay = monthlyPay(13.75,40);
-console.log(totalMonthlyPay);
+*/
