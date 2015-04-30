384e94689286b251182e9351faabc299123de398
RoddChames <Webmaster@Roddrecuss-MacBook-Pro.local>
2015-04-30 00:00:00 -0400

    Validation
    I’ve added the validation loop for the type of car the user wants.

--- Work Template/js/script.js
+++ Work Template/js/script.js
@@ -3,7 +3,7 @@
  * Functions Assignment
  */
 
-alert("This calculator will help determine how much car payments would be for a new car.");
+//alert("This calculator will help determine how much car payments would be for a new car.");
 
 var question1 = prompt("Please enter your name to help us better serve you");
 console.log(question1);
@@ -11,16 +11,17 @@ console.log(question1);
 var question2 = prompt("Hello " +question1+ " what kind of car would you like to purchase?\n(Dodge Charger, Chevy Camaro, Ford Mustang)").toLowerCase();
 console.log(question2);
 
-if(question2 === "dodge charger" ){
-    console.log("The price the selected car is $" +42,500+ ".");
-}else if(question2 === "chevy camaro"){
-    console.log("The price the selected car is $" +38,200+ ".");
-}else if(question2 === "ford mustang"){
-    console.log("The price the selected car is $" +37,800+ ".");
+while(question2 != "dodge charger" && question2 != "chevy camaro" && question2 != "ford mustang"){
+    question2 = prompt("You have entered an invalid selection. Please select from the following:\nDodge Charger, Chevy Camaro, or Ford Mustang.").toLowerCase();
+    console.log(question2);
 }
 
-while(){
-
+if(question2 === "dodge charger" || tryAgain === "dodge charger" ){
+    console.log("The price the selected car is $" +42,500+ ".");
+}else if(question2 === "chevy camaro" || tryAgain === "chevy camaro" ){
+    console.log("The price the selected car is $" +38,200+ ".");
+}else if(question2 === "ford mustang" || tryAgain === "ford mustang" ){
+    console.log("The price the selected car is $" +37,800+ ".");
 }
 
 
